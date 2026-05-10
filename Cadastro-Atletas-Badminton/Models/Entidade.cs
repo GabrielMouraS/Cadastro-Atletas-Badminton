c8011970bff8aaa27a049bd4454f8f6ddfbc234e
Gabriel Moura <gabrielmouragtxgv@gmail.com>
2026-05-10 20:15:37 -0300

    correcao

--- Cadastro-Atletas-Badminton/Models/Entidade.cs
+++
@@ -1,9 +0,0 @@
-namespace BadmintonCadastro.Models;
-
-public class Entidade
-{
-    public int Id { get; set; }
-    public string Sigla { get; set; } = string.Empty;
-    public string NomeCompleto { get; set; } = string.Empty;
-    public string? Cidade { get; set; }
-}
